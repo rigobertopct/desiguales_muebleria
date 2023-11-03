fca696d11a68fa809da8d488727caa6bc5efb2c2
Rigo <rigosofttech@gmail.com>
2023-11-03 19:52:13 -0400

    cxn de bd

--- config/settings.py
+++ config/settings.py
@@ -76,11 +76,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'muebles',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
